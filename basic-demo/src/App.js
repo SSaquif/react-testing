cee1109997ccbb6396e5b0921d2f339df99b6ba5
Sadnan Saquif <ssaquif@gmail.com>
2021-07-09 01:36:19 -0400

    basic notes done

--- basic-demo/src/App.js
+++ basic-demo/src/App.js
@@ -2,11 +2,12 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Jest</h1>
-    </div>
-  );
+  // return (
+  //   <div className="App">
+  //     {/* <h1>Jest</h1> */}
+  //   </div>
+  // );
+  // return <></>;
 }
 
 export default App;
--- basic-demo/src/App.test.js
+++ basic-demo/src/App.test.js
@@ -1,8 +1,24 @@
-import { render, screen } from "@testing-library/react";
 import App from "./App";
+import Enzyme, { shallow } from "enzyme";
+import EnzymeAdapter from "@wojtekmaj/enzyme-adapter-react-17";
 
-test("renders learn react link", () => {
-  render(<App />);
-  const linkElement = screen.getByText(/jest/i);
-  expect(linkElement).toBeInTheDocument();
+Enzyme.configure({ adapter: new EnzymeAdapter() });
+
+// test() is provided by Jest
+test("renders without crashing", () => {
+  const wrapper = shallow(<App />);
+  // returns the html in our wrapper (App node)
+  console.log(wrapper.debug());
+  // Asserstions like expect are part of Jest
+  // expect();
+});
+
+test("renders non-emty component without crashing", () => {
+  const wrapper = shallow(<App />);
+  // Asserstions like expect are part of Jest
+  // The expect checks if any nodes exist in our shallow wrapper
+  // If no nodes exist the wrapper is empty
+  // Confusion: When I removed everything in App. It still passed test
+  // Read up on the exists function a bit more in enzyme docs
+  expect(wrapper.exists()).toBe(true);
 });
