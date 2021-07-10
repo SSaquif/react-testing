7f8b42b2c47621aed16f60fe0252b088828de98d
Sadnan Saquif <ssaquif@gmail.com>
2021-07-09 20:49:20 -0400

    basics of enzyme and jest completed

--- click-counter/src/App.js
+++ click-counter/src/App.js
@@ -1,8 +1,24 @@
 import logo from "./logo.svg";
 import "./App.css";
+import { useState } from "react";
 
 function App() {
-  return <div data-test="component-app"></div>;
+  const [count, setCount] = useState(0);
+
+  const handleClick = () => {
+    setCount(count + 1);
+  };
+
+  return (
+    <div data-test="app-component">
+      <h1 data-test="counter-display">
+        Current Counter:<span data-test="count">{count}</span>
+      </h1>
+      <button onClick={handleClick} data-test="increment-button">
+        Click Me
+      </button>
+    </div>
+  );
 }
 
 export default App;
--- click-counter/src/App.test.js
+++ click-counter/src/App.test.js
@@ -1,25 +1,56 @@
-import Enzyme, { shallow } from "enzyme";
+import Enzyme, { shallow, ShallowWrapper } from "enzyme";
 import EnzymeAdapter from "@wojtekmaj/enzyme-adapter-react-17";
 import App from "./App";
 
 Enzyme.configure({ adapter: new EnzymeAdapter() });
 
+/**
+ * Factory function to create a ShallowWrapper for a component
+ * @param {*} component
+ * @returns {ShallowWrapper}
+ */
+const setup = (component) => shallow(component);
+
+const findByTestAttr = (wrapper, val) => {
+  return wrapper.find(`[data-test='${val}']`);
+};
+
 test("renders without err", () => {
   // create shallow wrapper for APP
-  const wrapper = shallow(<App />);
+  const wrapper = setup(<App />);
   // find elements with the sepecified attribute
-  const appComponent = wrapper.find("[data-test='component-app']");
+  const appComponent = findByTestAttr(wrapper, "app-component");
   // find returns all elements that match
   //we should have exactly one match
   expect(appComponent.length).toBe(1);
 });
 
-test("renders increment button", () => {});
+test("renders increment button", () => {
+  const wrapper = setup(<App />);
+  const incrBtn = findByTestAttr(wrapper, "increment-button");
+  expect(incrBtn.length).toBe(1);
+});
 
-test("renders counter display", () => {});
+test("renders counter display", () => {
+  const wrapper = setup(<App />);
+  const cntDisplay = findByTestAttr(wrapper, "counter-display");
+  expect(cntDisplay.length).toBe(1);
+});
 
-test("counter display starts at 0", () => {});
+test("counter display starts at 0", () => {
+  const wrapper = setup(<App />);
+  const count = findByTestAttr(wrapper, "count").text(); //returns string
+  expect(count).toBe("0");
+});
 
-// Here we will test that it increments the counter display, ie: test behaviour/functionality
-// And NOT test the state, ie that it increments counter state that is NOT test implementation
-test("clicking button increments counter display", () => {});
+// Here we will test that it increments the counter display,
+// ie: test behaviour/functionality
+// And NOT test the state,
+//ie that it increments counter state (NOT test implementation)
+test("clicking button increments counter display", () => {
+  const wrapper = setup(<App />);
+  const incrBtn = findByTestAttr(wrapper, "increment-button");
+  incrBtn.simulate("click");
+  const count = findByTestAttr(wrapper, "count").text();
+  expect(count).toBe("1");
+});
